58d3c1c3c1a084c3d036631497ad1c98cd46d5ce
Nunzio Baldares <bvldares@outlook.com>
2022-12-21 16:47:12 +0100

    discount and bg fix

--- interactive-pricing-component/src/Components/Form.jsx
+++ interactive-pricing-component/src/Components/Form.jsx
@@ -6,15 +6,14 @@ import "../App.css"
 export default function Form(){
     
     const[price, setPrice] = useState(55)
+    const[yearlyBilling, setYearlyBilling] = useState(false)
+    
+    function handleChange(e){setPrice(e.target.value)}
+    function placeOrder(){console.log("orderDone")}
 
-    
-    function handleChange(e){
-        setPrice(e.target.value)
-    }
-    
-    function placeOrder(){
-        console.log("orderDone")
-    }
+    function toggleDiscount(){setYearlyBilling(prev => !prev)}
+
+
     
     return (
         <form onSubmit={placeOrder}>
@@ -22,12 +21,15 @@ export default function Form(){
             <div className="form-pricing">
             <h4 className="views">{(price * 2.5).toFixed(2)}K PAGEVIEWS</h4>
             <input className="range-input" type="range"  min="5" max="100" step="1" value={price} onChange={handleChange}/>
-            <h3 className="price">${parseFloat(price).toFixed(2)} <span>/ month</span></h3>
+            <h3 className="price">
+                ${parseFloat(yearlyBilling? price-(price *0.25): price).toFixed(2)} 
+                
+                <span>/ month</span></h3>
             </div>
 
             <div className="form-period">
                 <p className="period">Monthly Billing</p>
-                <input type="checkbox" id="switch" /><label htmlFor="switch">Toggle</label>
+                <input type="checkbox" id="switch" checked={yearlyBilling} onChange={toggleDiscount}/><label htmlFor="switch">Toggle</label>
                 <p className="period yearly">Yearly Billing</p>
                 
             </div>
